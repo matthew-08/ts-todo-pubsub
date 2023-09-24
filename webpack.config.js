97986d42eb396c443ce6205fd65e035b194c0b70
Matthew <matthewjcrosby1@gmail.com>
2023-09-23 20:24:01 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.ts',
+  mode: 'development',
   module: {
     rules: [
       {
@@ -17,5 +20,12 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  devServer: {
+    watchFiles: ["src/*.html"],
+    hot: true,
   },
 };
